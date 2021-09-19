383a48c8b350a43dfb3dad82a8509251105498cb
Fábio dos Santos <fah_ds@live.com>
2021-09-19 14:35:56 -0300

    instanceof examples

--- example.js
+++ example.js
@@ -1,16 +1,27 @@
-const createPerson = function (name, city, year) {
-  return {
-    name,
-    city,
-    year,
-    getAge() {
-      return new Date().getFullYear() - this.year;
-    },
-  };
+const Person = function (name, city, year) {
+  this.name = name;
+  this.city = city;
+  this.year = year;
+  this.age = this.getAge();
 };
 
-const person1 = createPerson("Linus Torvalds", "Helsinky", 1969);
-const person2 = createPerson("Bill Gates", "Seattle", 1955);
+Person.prototype.getAge = function () {
+  return new Date().getFullYear() - this.year;
+};
 
-console.log(person1.getAge());
+const person1 = new Person("Linus Torvalds", "Helsinky", 1969);
+const person2 = new Person("Bill Gates", "Seattle", 1955);
+
+console.log(person1);
+console.log(person1.age);
+console.log(person2);
 console.log(person2.getAge());
+
+const _new = (fn, ...params) => {
+  const obj = {};
+  Object.setPrototypeOf(obj, fn.prototype);
+  fn.apply(obj, params);
+  return obj;
+};
+const p1 = _new(Person, "Linus Torvalds", "Helsinky", 1969);
+console.log(p1, p1.getAge());
